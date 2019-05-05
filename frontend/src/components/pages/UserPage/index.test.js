68318f2287141d861c3d44bb7b0d3e1b6f06d876
Skile <imleejm@gmail.com>
2019-05-05 17:58:19 +0900

    Modify UserPage test to accept username props

--- frontend/src/components/pages/UserPage/index.test.js
+++ frontend/src/components/pages/UserPage/index.test.js
@@ -2,6 +2,11 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import UserPage from '.'
 
-it('renders', () => {
-  shallow(<UserPage />)
+const wrap = (props = {}) => shallow(<UserPage {...props} />)
+
+describe('userPage', () => {
+  it('renders username correctly', () => {
+    const wrapper = wrap({ username: 'user1' })
+    expect(wrapper.contains('user1')).toBe(true)
+  })
 })
