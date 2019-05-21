253c0f5ec914603fa1c19c0c8dbaf388b902b6ea
jtjun <tae7130@naver.com>
2019-05-21 13:42:25 +0900

    minor bug fix

--- backend/src/dutch_broomstick/models.py
+++ backend/src/dutch_broomstick/models.py
@@ -10,11 +10,12 @@ class Profile(models.Model):
 class Room(models.Model):
     roomname = models.CharField(max_length=30, blank=False) # Room's name
     # Room's owner is always registered user
-    owner = models.OneToManyField(User, on_delete=models.CASCADE, null=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    # ForeignKey can handle OneToMany
 
 class Member(models.Model):
     name = models.CharField(max_length=30, blank=False)
-    account = models.CharField(blank=True)
-    room = models.OneToManyField(Room, on_delete=models.CASCADE, null=False)
-    user = models.OneToManyField(User, on_delete=models.CASCADE, null=True) # user is nullable
+    account = models.CharField(max_length=30, blank=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) # user is nullable
 
