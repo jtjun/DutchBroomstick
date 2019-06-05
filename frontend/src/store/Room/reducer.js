cb7a0be622f3213623b367d9a5fca60418d515ab
y123ob <y123ob@naver.com>
2019-06-05 15:56:57 +0900

    to solve conflict

--- frontend/src/store/Room/reducer.js
+++ frontend/src/store/Room/reducer.js
@@ -1,34 +1,58 @@
-import {
-    ROOM_CREATE_REQUEST,
-    ROOM_SETTING_REQUEST,
-  } from 'store/actions'
-import { ROOM_CREATE_SUCCESS, } from './actions';
-  
-  const initialState = {
-    roomname: null,
-  }
-  
-  const roomReducer = (state = initialState, action) => {
-    switch(action.type) {
-      
-      case ROOM_CREATE_REQUEST:
-        return{
-          state,
-        }
-      case ROOM_CREATE_SUCCESS:
-        return{
-          ...state,
-          roomname: action.roomname,
-        }
-      case ROOM_SETTING_REQUEST:
-        return{
-          ...initialState
-        }
+import * as actions from './actions';
 
-      default:
-        return state
-    }
+const initialState = {
+  /* room: 현재 접속해있는 방의 정보 */
+  room: null,
+  /* roomList: UserPage의 방 목록 (해당 User가 접근할 수 있는 방 목록)
+   *   - 초기화 이전에는 null
+   *   - 초기화 된 이후에는 {roomname, url, owner}의 List로 구성됨
+   */
+  roomList: null,    
+}
+
+const roomReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case actions.ROOM_CREATE_REQUEST:
+      return {
+        ...state,
+      }
+    case actions.ROOM_CREATE_SUCCESS:
+      return {
+        ...state,
+        room: action.room,
+        roomList: [
+          ...state.roomList,
+          action.room,
+        ],
+      }
+    case actions.ROOM_LIST_SUCCESS:
+      return {
+        ...state,
+        roomList: action.roomList,
+      }
+    case actions.ROOM_LIST_FAILED:
+      return {
+        ...state,
+        roomList: [],  // null 대신 빈 배열을 넣어 다시 호출이 일어나지 않도록 했음
+      }
+    case actions.ROOM_GET_SUCCESS:
+      return {
+        ...state,
+        room: action.room,
+      }
+    case actions.ROOM_GET_FAILED:
+      return {
+        ...state,
+        room: null,
+      }
+    case actions.ROOM_LEAVE:
+      return {
+        ...state,
+        room: null,
+      }
+    default:
+      return state
   }
-  
-  export default roomReducer
-  
+}
+
+export default roomReducer
