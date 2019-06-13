1f2f10901681db8a68ab0b2e8c28726fae11e59b
y123ob <y123ob@naver.com>
2019-06-14 03:48:01 +0900

    rechange store folders' Capital...

---
+++ frontend/src/store/Room/actions.js
@@ -0,0 +1,77 @@
+/*Room Create Action */
+export const ROOM_CREATE_REQUEST = "ROOM_CREATE_REQUEST"
+export const ROOM_CREATE_SUCCESS = "ROOM_CREATE_SUCCESS"
+
+export const roomCreateRequest = (roomname, users, username, token) => ({
+  type: ROOM_CREATE_REQUEST,
+  roomname,
+  users,
+  username,
+  token,
+})
+
+export const roomCreateSuccess = (room) => ({
+  type: ROOM_CREATE_SUCCESS,
+  room,
+})
+
+/* Entrance , Setting neet to change*/
+export const ENTRANCE_REQUEST = "ENTRANCE_REQUEST"
+
+export const entranceRequest = () => ({
+    type: ENTRANCE_REQUEST,
+})
+
+export const ROOM_SETTING_REQUEST = "ROOM_SETTING_REQUEST"
+
+export const roomSettingRequest = () => ({
+    type: ROOM_SETTING_REQUEST,
+})
+
+
+/* Get Room List by User */
+export const ROOM_LIST_REQUEST = "ROOM_LIST_REQUEST"
+export const ROOM_LIST_SUCCESS = "ROOM_LIST_SUCCESS"
+export const ROOM_LIST_FAILED = "ROOM_LIST_FAILED"
+
+export const roomListRequest = (username, token) => ({
+  type: ROOM_LIST_REQUEST,
+  username,
+  token,
+})
+
+export const roomListSuccess = (roomList) => ({
+  type: ROOM_LIST_SUCCESS,
+  roomList,
+})
+
+export const roomListFailed = (error) => ({
+  type: ROOM_LIST_FAILED,
+  error,
+})
+
+
+/* Get Room by URL */
+export const ROOM_GET_REQUEST = "ROOM_GET_REQUEST"
+export const ROOM_GET_SUCCESS = "ROOM_GET_SUCCESS"
+export const ROOM_GET_FAILED = "ROOM_GET_FAILED"
+
+export const roomGetRequest = (url) => ({
+  type: ROOM_GET_REQUEST,
+  url,
+})
+
+export const roomGetSuccess = (room) => ({
+  type: ROOM_GET_SUCCESS,
+  room,
+})
+
+export const roomGetFailed = (error) => ({
+  type: ROOM_GET_FAILED,
+  error,
+})
+
+
+export const ROOM_LEAVE = "ROOM_LEAVE"
+
+export const roomLeave = () => ({ type: ROOM_LEAVE })
---
+++ frontend/src/store/Room/sagas.js
@@ -0,0 +1,66 @@
+import { fork, takeEvery, put } from 'redux-saga/effects'
+import { toastr } from 'react-redux-toastr'
+import api from 'services/api'
+
+/**
+ * https://github.com/diegohaz/arc/wiki/Sagas
+ */
+
+import * as actions from './actions'
+
+
+/* Room Create Request */
+
+function* roomCreateRequest({ roomname, users, username, token }){
+  try {
+    const room = yield api.post(`/users/${username}/rooms/`, { roomname }, { token })
+    toastr.light(
+      `${room.roomname}`, '새로운 방이 만들어졌습니다!',
+      { icon: 'success', status: 'success', }
+    )
+    yield put(actions.roomCreateSuccess(room))
+  } catch(e) {
+    console.log(e)
+  }
+}
+function* watchCreatePageRequest() {
+  yield takeEvery(actions.ROOM_CREATE_REQUEST, roomCreateRequest)
+}
+
+
+/* Room List Request */
+function* listRequest({ username, token }) {
+  try {
+    const roomList = yield api.get(`/users/${username}/rooms/`, { token })
+    yield put(actions.roomListSuccess(roomList))
+  } catch(e) {
+    yield put(actions.roomListFailed(e))
+  }
+}
+
+function* watchRoomListRequest() {
+  yield takeEvery(actions.ROOM_LIST_REQUEST, listRequest)
+}
+
+
+/* Room Get Request */
+function* getRequest({ url }) {
+  try {
+    const room = yield api.get(`/rooms/${url}/`)
+    yield put(actions.roomGetSuccess(room))
+  } catch(e) {
+    yield put(actions.roomGetFailed(e))
+  }
+}
+
+function* watchRoomGetRequest() {
+  yield takeEvery(actions.ROOM_GET_REQUEST, getRequest)
+}
+
+
+export default function* () {
+  yield fork(watchCreatePageRequest)
+  yield fork(watchRoomListRequest)
+  yield fork(watchRoomGetRequest)
+}
+
