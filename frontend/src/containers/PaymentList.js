f28f8cd3e86c7d43b239fe8d41e157f9067b03ab
Skile <imleejm@gmail.com>
2019-06-15 23:58:44 +0900

    Rename PaymentListPage to PaymentList (in RoomPage)

--- frontend/src/containers/PaymentList.js
+++ frontend/src/containers/PaymentList.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { PaymentListPage } from 'components'
+import { PaymentList } from 'components'
 import { Redirect } from 'react-router-dom'
 
-const PaymentListPageContainer = (props) => {
+const PaymentListContainer = (props) => {
   
   
   if( !props.roomname ){
@@ -13,7 +13,7 @@ const PaymentListPageContainer = (props) => {
   } 
   else {
     return (
-      <PaymentListPage {...props} />
+      <PaymentList {...props} />
     )
   }
 
@@ -22,8 +22,8 @@ const PaymentListPageContainer = (props) => {
 const mapStateToProps = (state) => ({
     username : state.user.username,
     roomname : state.room.room.roomname,
-    paymentlist : state.Payment.paymentlist,
-    roomurl : state.room.room.url
+    paymentlist : state.payment.paymentlist,
+    roomurl : state.room.room.url,
 })
 
-export default connect(mapStateToProps)(PaymentListPageContainer)
+export default connect(mapStateToProps)(PaymentListContainer)
