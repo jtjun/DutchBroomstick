b5eaaf7ace3dc2f9b9aac9cf2b6e22159a4ade1a
y123ob <y123ob@naver.com>
2019-06-19 20:27:14 +0900

    AccountPage bug

--- frontend/src/components/pages/AccountPage/index.js
+++ frontend/src/components/pages/AccountPage/index.js
@@ -4,31 +4,44 @@ import { Link } from 'react-router-dom'
 import { Button, Header, List, Block } from 'components'
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-const AccountPage = ({ toname, money, account }) => (
-    //toname = '정유석',
-    //money = '10000',
-    //account = '국민 353901-01-142611',
-    
-    <div>
-        <Header />
-        <Block>
-            <h3>To.{toname}</h3>
-            <h3>{money}원</h3>
-            <h4>{account}</h4>
-        </Block>
+const AccountPage = ({ match, roomname, sendlist, members }) => {
+    const to = match.params.to
+    const toname = to
+    console.log(toname)
+    console.log(sendlist)
+    const edge = sendlist.find(m=> m.to == toname)
+    console.log(edge)
+    const member = members.find(m=> m.membername == toname)
+    console.log(member)
+    const money = edge.label
+    const account = member.account
+    console.log(account)
+    return (
+        //toname = '정유석',
+        //money = '10000',
+        //account = '국민 353901-01-142611',
+        
+        <div>
+            <Header />
+            <Block>
+                <h3>To.{toname}</h3>
+                <h3>{money}원</h3>
+                <h4>{account}</h4>
+            </Block>
 
-        <CopyToClipboard 
-            text={`${account} ${toname}`}
-            onCopy={() => { }}>
-            <span>
-                <Button>
-                    Copy Account!
-                </Button>
-            </span>
-        </CopyToClipboard>
+            <CopyToClipboard 
+                text={`${account} ${toname}`}
+                onCopy={() => { }}>
+                <span>
+                    <Button>
+                        Copy Account!
+                    </Button>
+                </span>
+            </CopyToClipboard>
 
-    </div>
-)
+        </div>
+    )
+}
 
 
 export default AccountPage
--- frontend/src/containers/AccountPage.js
+++ frontend/src/containers/AccountPage.js
@@ -3,13 +3,15 @@ import { connect } from 'react-redux'
 import { AccountPage } from 'components'
 import { Redirect } from 'react-router-dom'
 
+
 class AccountPageContainer extends React.Component {
   
+  /*
   componentDidMount(){
     const { roomname, toname, member, money, account, onAccoutPage } = this.props
-    onAccoutPage(toname)
+    onAccoutPage(toname,member)
   }
-
+  */
   render(){
     
     /*
@@ -31,7 +33,7 @@ class AccountPageContainer extends React.Component {
     }
     */
     return (
-        <AccountPage {...props} />
+        <AccountPage {...this.props} />
     )
 
   }
@@ -41,16 +43,19 @@ class AccountPageContainer extends React.Component {
 const mapStateToProps = (state) => ({
     
     roomname : state.room.room.roomname,
+    sendlist: state.payment.sendlist,
+    members : state.member.members
 
-    toname : state.payment.senddata.to,
-    member: state.member.members,
-    money : state.payment.senddata.label,
-    account : state.payment.account,
+    //toname : state.payment.senddata.to,
+    //member: state.member.members,
+    
+    //money : state.payment.senddata.label,
+    //account : state.payment.account,
 
 })
 
 const mapDispatchToProps = dispatch => ({
-  onAccoutPage: (toname,member) => dispatch(accountinRequest(toname,member)),
+  //onAccoutPage: (toname,member) => dispatch(accountinRequest(toname,member)),
 })
 
 export default connect(mapStateToProps,mapDispatchToProps)(AccountPageContainer)
